c915cfdb1ab29df2e6e53c814315166d0a6083d5
rahatfaruk <rahatfaruk99@gmail.com>
2024-05-11 20:21:51 +0600

    ErrorPage added

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Home from './pages/Home/index.jsx'
-import Login from './pages/Login.jsx'
-import Queries from './pages/Queries/index.jsx'
+// context, css
 import ThemeProvider from './context/ThemeProvider.jsx'
+import './index.css'
+// pages, components
+import App from './App.jsx'
+import Home from './pages/Home'
+import Login from './pages/Login.jsx'
+import Queries from './pages/Queries'
+import ErrorPage from './pages/ErrorPage.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
+    <Route path='/' element={<App/>} errorElement={<ErrorPage/>}>
       <Route index element={<Home/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/queries' element={<Queries/>} />
