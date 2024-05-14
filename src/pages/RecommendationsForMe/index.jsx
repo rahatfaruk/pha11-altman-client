c026762a119d8f73317ec449a6288c58ef9674b3
rahatfaruk <rahatfaruk99@gmail.com>
2024-05-14 20:11:00 +0600

    RecommendForMe connect with server

--- src/pages/RecommendationsForMe/index.jsx
+++ src/pages/RecommendationsForMe/index.jsx
@@ -3,16 +3,32 @@ import axios from "axios";
 import { maxContent } from "../../App";
 import SectionTitle from "../../comps/SectionTitle";
 import Table from "./Table";
+import useAxios from "../../hooks/useAxios";
+import useAuth from "../../hooks/useAuth";
+import Loading from "../../comps/Loading";
 
 function RecommendationsForMe() {
   const [recommendations, setRecommendations] = useState([])
+  const [loading, setLoading] = useState(true)
+  const {axiosBase} = useAxios()
+  const {user} = useAuth()
 
+  // get recommendations for me (where others are recommender)
   useEffect(() => {
-    axios('/data2.json')
+    axiosBase(`/all-recommendations?userEmail=${user.email}`)
     .then(res => {
       setRecommendations(res.data)
+      setLoading(false)
     })
-  })
+    .catch(err => {
+      setLoading(false)
+      toast.error('fetching comments failed!' + err.message)
+    })
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (  
     <section className="flex-1 px-4 dark:bg-gray-800 dark:text-white">
